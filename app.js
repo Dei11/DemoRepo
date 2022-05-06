b9cbc0824a415eec3c1db45e4194445edf00fe14
Andrei Bogdan <dei7bogdan@gmail.com>
2022-05-06 22:23:10 +0300

    app.js changed

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+for(let i = 1; i <= 10; i++){
+    let string = "";
+    if(i%3 == 0){
+        string += "Fizz";
+    }
+    else if(i%5 == 0){
+        string += "Buzz";
+    }
+    console.log(i + ": " + string);
+}
